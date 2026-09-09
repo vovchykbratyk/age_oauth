9cc5969d4abf2996d21bbfd915cde8fcd02ce64c
Eric Eagle <40668075+vovchykbratyk@users.noreply.github.com>
2026-09-09 16:57:37 -0400

    expose Connection API

--- src/age_oauth/__init__.py
+++ src/age_oauth/__init__.py
@@ -1,4 +1,11 @@
 from .oauth import OAuthConfig, AGEOAuth, get_gis
 from .connections import ConnectionStore
+from .connection import Connection
 
-__all__ = ["OAuthConfig", "AGEOAuth", "get_gis", "ConnectionStore"]
+__all__ = [
+    "OAuthConfig",
+    "AGEOAuth",
+    "get_gis",
+    "ConnectionStore",
+    "Connection",
+]
